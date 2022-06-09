f3cedfe784fa37756f3606e8c392d3bb0459e6b7
Mücahit Nezir <mucahitnezir@gmail.com>
2022-06-10 00:16:34 +0300

    Update master page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
+import { meta } from '../site.config'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+
+        <link rel="canonical" href={meta.url} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col">
+        <main className="flex-grow bg-amber-50">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default MyApp
---
+++ site.config.js
@@ -0,0 +1,16 @@
+export const meta = {
+  title: 'Mücahit Nezir',
+  description: 'Software engineer from Turkey',
+  url: 'https://mucahitnezir.com',
+  author: {
+    name: 'Mücahit Nezir',
+    email: 'mucahitnezir@gmail.com'
+  },
+  social: {
+    twitter: 'https://twitter.com/mucahitnezir',
+    linkedin: 'https://www.linkedin.com/in/mucahitnezir',
+    github: 'https://github.com/mucahitnezir',
+    medium: 'https://mucahitnezir.medium.com',
+    instagram: 'https://www.instagram.com/mucahitnezir'
+  }
+}
