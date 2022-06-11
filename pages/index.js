f7d4989934eaf494d1eab435e9a47f66e558f1d3
Mücahit Nezir <mucahitnezir@gmail.com>
2022-06-11 13:21:04 +0300

    Remove title tag from homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-
 import Banner from '../components/banner'
 import Social from '../components/social'
 
 function Home() {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-
       <Banner />
       <Social />
     </>
