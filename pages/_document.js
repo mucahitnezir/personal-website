916351dc4c563b5230765445a5f5508c0cf34f81
Mücahit Nezir <mucahitnezir@gmail.com>
2022-06-11 20:34:14 +0300

    #4 add favicon and webmanifest file

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,10 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#ebebeb" />
 
           {/* icons */}
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="manifest" href="/icons/site.webmanifest" />
         </Head>
         <body className="bg-gray-100">
           {GTM_AVAILABLE && (
